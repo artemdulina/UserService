b0580d35bb097ebb56ba6bc81d1b745f3c3c297a
artemdulina <artem.dulina@gmail.com>
2016-12-13 01:28:29 +0300

    started to work with wcf

---
+++ UserServiceTester/MasterServiceClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MasterServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1Client client = new Service1Client();
+
+            Console.WriteLine(client.GetData(10));
+
+            client.Close();
+        }
+    }
+}
